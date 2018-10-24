cf35a52d9de6e59970c7164368680480af57ccd6
Robin Fröjd <robinae@hotmail.se>
2018-10-25 00:17:29 +0200

    Update esp_receive.ino
    speed improvements, still need to clean up

--- Espnow/esp_receive.ino
+++ Espnow/esp_receive.ino
@@ -5,8 +5,14 @@ extern "C" {
   #include <espnow.h>
   #include "user_interface.h"
 }
-int count = 0;
-
+#define MSG_LEN 40
+struct __attribute__((packed)) MSG {                                          
+  char text[MSG_LEN];
+  unsigned long value;
+  unsigned int led;
+} msg;
+int msgSize = sizeof(msg);
+volatile boolean haveMsg = false;                                               // Volatile flag when new msg is received
 /* Set a private Mac Address
  *  http://serverfault.com/questions/40712/what-range-of-mac-addresses-can-i-safely-use-for-my-virtual-machines
  * Note: the point of setting a specific MAC is so you can replace this Gateway ESP8266 device with a new one
@@ -24,48 +30,32 @@ void setup() {
   Serial.print("This node AP mac: "); Serial.println(WiFi.softAPmacAddress());
   Serial.print("This node STA mac: "); Serial.println(WiFi.macAddress());
 
-  initEspNow();
+  if (esp_now_init() != 0) {                                                    // Halt execution if init failed
+    Serial.println("ESP-NOW init failed");
+    while(1);
+  }
+  esp_now_set_self_role(ESP_NOW_ROLE_SLAVE);                                    // slave is receiver only
+  esp_now_register_recv_cb([](uint8_t *mac, uint8_t *data, uint8_t len) {       // call back (interrupt) when
+                                                            // message is received stores
+  
+    memcpy(&msg, data, sizeof(msg));                                            // call back also stores the msg
+    haveMsg = true;                                                             // flag that msg was received
+  });
 }
 
-int heartBeat;
 
 void loop() {
-  if (millis()-heartBeat > 30000) {
-    Serial.println("Waiting for ESP-NOW messages...");
-    heartBeat = millis();
+    if (haveMsg == true){                                                         // If a msg has been received
+    
+    //Serial.print(msg.text);
+    Serial.println(msg.value);
+
+
+    
+    }
+    
+    haveMsg = false;                                                           // and set the msg flag to false
   }
-}
 
-void initEspNow() {
-  if (esp_now_init()!=0) {
-    Serial.println("*** ESP_Now init failed");
-    ESP.restart();
-  }
 
-  esp_now_set_self_role(ESP_NOW_ROLE_COMBO);
-
-  esp_now_register_recv_cb([](uint8_t *mac, uint8_t *data, uint8_t len) {
-
-    // Investigate: There's little doc on what can be done within this method. If its like an ISR
-    // then it should not take too long or do much I/O, but writing to Serial does appear to work ok
-
-    //Serial.print("$$"); // $$ just an indicator that this line is a received ESP-Now message
-    //Serial.write(mac, 6); // mac address of remote ESP-Now device
-    //Serial.write(len);
-     //Serial.print(data);
-    //Serial.write(data, len); 
-
-     
- //String s;
-   //   for (int i=0; i < len; i++) s += char(data[i]);
-     // Serial.println(s); 
-
-String s = String(((char*)data));
-      //Serial.println(s);
-      Serial.println(count);
-      
-      count += 1;
-     
-  });
   
-}
