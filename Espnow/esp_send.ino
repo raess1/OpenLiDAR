a46b53ad2efa4e25d0eb7044d0f49c41234a920a
Robin Fröjd <robinae@hotmail.se>
2018-10-25 00:16:57 +0200

    Update esp_send.ino
    speed improvements. still need to clean up

--- Espnow/esp_send.ino
+++ Espnow/esp_send.ino
@@ -1,7 +1,8 @@
 /*
-
  */
 #include <ESP8266WiFi.h>
+#include <Wire.h>
+#include <LIDARLite.h>
 extern "C" {
 #include <espnow.h>
 }
@@ -10,20 +11,31 @@ extern "C" {
 // Change the MAC Address to the receiver ESP(s) that will receive the messages from your sending ESP8266
 uint8_t recMAC[] = {0x36, 0x33, 0x33, 0x33, 0x33, 0x33};
 
-#define WIFI_CHANNEL 1                                                         // Set channel from 1 to 14                                                        
+#define WIFI_CHANNEL 1                                                         // Set channel from 1 to 14      
+                                                  
 
-// Set up a struct to store the text and numbers that will be sent - use packed for the smallest possible alignment for bytes
-// All receiving ESP(s) must also have this structure.
-
-#define MSG_LEN 15
+#define MSG_LEN 40
 struct __attribute__((packed)) MSG {                                          
   char text[MSG_LEN];
-  unsigned long number;
-} msg;
-int msgSize = sizeof(msg);                                                      // NOTE:  must be less than 250 bytes!
+  unsigned long value;
+};
+MSG radarMsg;
+int msgSize = sizeof(radarMsg);                                                  // NOTE:  must be less than 250 bytes!
+
+//lidar
+boolean            lastRadarState = 0;
+unsigned long      radarCount = 0;
+LIDARLite myLidarLite;
+
+
 
 void setup() {
+
+  Wire.begin(D4, D1);
   Serial.begin(500000);
+  myLidarLite.begin(0, true); // Set configuration to default and I2C to 400 kHz
+  myLidarLite.configure(0); // Change this number to try out alternate configurations
+
   Serial.println("\nStarting ESP-NOW Sender");
   
   WiFi.mode(WIFI_STA);                                                          // Place in station mode for ESP-NOW sensor node
@@ -48,18 +60,24 @@ void setup() {
   }
   esp_now_set_self_role(ESP_NOW_ROLE_CONTROLLER);                               // Sender is in controller role
   esp_now_add_peer(recMAC, ESP_NOW_ROLE_SLAVE, WIFI_CHANNEL, NULL, 0);          // Sends to MAC of receiver in slave role
-                                                                                // Note that multiple peers can be added
-  strcpy(msg.text, "Hello World: ");                                            // Initialize the text message                                 
-  msg.number = 0;                                                               // and number to be sent
+
+
+
 }
 
-void loop() {
-  //Serial.print(msg.text);
-  Serial.println(msg.number);                                                                                                
-  unsigned char store[msgSize];                                                 // messages are stored as unsigned char
-  memcpy(&store, &msg, msgSize);                                                // for transmission (req'd by ESP-NOW)
-  esp_now_send(NULL, store, msgSize);                                           // NULL sends to all peers
 
+void loop() {
+
+  // Lidar
+  strcpy(radarMsg.text, ":");                                   // Initialize the text messages                                
+  radarMsg.value = myLidarLite.distance();                                                           // and values to be sent
+  //Serial.println("Lidar set up");
+  unsigned char store[msgSize];                                                     // ESP-NOW buffer stored as unsigned char
+
+      radarCount++;
+      //Serial.print(radarMsg.text);
+      //Serial.println(radarMsg.value);
+      memcpy(&store, &radarMsg, msgSize);                                           // for transmission (req'd by ESP-NOW)
+      esp_now_send(NULL, store, msgSize);                                           // NULL sends to all peers
   delay(0);
-  msg.number++;                                                                 // msg number is just a loop counter
 }
